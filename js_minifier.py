05bef2390fa0ec0028ea614a8b72e931bc33f0c1
李俊諭 JYUN-YU LI <s123600g@gmail.com>
2020-07-07 23:01:07 +0800

    Update js_minifier.py

--- js_minifier.py
+++ js_minifier.py
@@ -153,27 +153,3 @@ if __name__ == "__main__":
         LogHelper.info("執行異常，終止作業.")
         LogHelper.error("{}\n{}".format(err,err.with_traceback()))
 
-
-# try:
-#     js_file = sys.argv[1]
-# except:
-#     print("Missing input file")
-#     sys.exit()
-
-# # Grab the file contents
-# with open(js_file, 'r') as c:
-#     js = c.read()
-
-# # Pack it, ship it
-# payload = {'input': js}
-# url = 'https://javascript-minifier.com/raw'
-# print("Requesting mini-me of {}. . .".format(c.name))
-# r = requests.post(url, payload)
-
-# # Write out minified version
-# minified = js_file.rstrip('.js')+'.min.js'
-# with open(minified, 'w') as m:
-#     m.write(r.text)
-
-# print("Minification complete. See {}".format(m.name))
-
